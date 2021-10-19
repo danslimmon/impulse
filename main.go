a8e4188ff3b544a083d7bbdc797998c1d5840377
Dan Slimmon <dslimmon@hashicorp.com>
2021-10-19 05:27:05 -0400

    removed unused server.go

--- main.go
+++ main.go
@@ -1,37 +1,6 @@
 package main
 
-import (
-	"bytes"
-	"net/http"
-	"net/http/httptest"
-	"os"
-
-	log "github.com/sirupsen/logrus"
-
-	"github.com/danslimmon/impulse/server"
-)
-
 func main() {
-	mux, err := server.Start()
-	if err != nil {
-		log.WithField("error", err.Error()).Error("error starting server")
-		os.Exit(1)
-	}
-
-	w := new(httptest.ResponseRecorder)
-	buf := bytes.NewBuffer([]byte{})
-	w.Body = buf
-
-	r, _ := http.NewRequest("GET", "http://localhost/hello", nil)
-
-	mux.ServeHTTP(w, r)
-	/*
-		p := []byte{}
-		n, err := buf.Read(p)
-		if err != nil {
-			log.WithField("error", err).Error("wtf")
-			os.Exit(1)
-		}
-	*/
-	log.WithField("body", buf.String()).Info("response body")
+	// when we're ready (i.e. when ImpulseClient exists), this function will use an ImpulseClient to
+	// retrieve ~/i/_ and print it. for now, it does nothing.
 }
