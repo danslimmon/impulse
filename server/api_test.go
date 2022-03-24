5f74dd7dec6aef1d808f936c48d13f3ad811beb1
Dan Slimmon <dslimmon@hashicorp.com>
2022-03-23 21:00:10 -0400

    beef up GetTask testing

--- server/api_test.go
+++ server/api_test.go
@@ -21,7 +21,7 @@ func TestGetTask(t *testing.T) {
 	}
 	apiResp := new(GetTaskResponse)
 	err := s.GetTask(apiReq, apiResp)
-	assert.Equal(nil, err)
+	assert.Nil(err)
 
 	makePasta := common.MakePasta()[0]
 	assert.Equal(1, len(apiResp.Tasks))
@@ -32,7 +32,7 @@ func TestGetTask(t *testing.T) {
 	apiReq.TaskIDs = []string{}
 	apiResp = new(GetTaskResponse)
 	err = s.GetTask(apiReq, apiResp)
-	assert.Equal(nil, err)
+	assert.Nil(err)
 	assert.Equal(0, len(apiResp.Tasks))
 
 	// Multiple task IDs supplied
@@ -44,7 +44,7 @@ func TestGetTask(t *testing.T) {
 	}
 	apiResp = new(GetTaskResponse)
 	err = s.GetTask(apiReq, apiResp)
-	assert.Equal(nil, err)
+	assert.Nil(err)
 
 	multipleNested := common.MultipleNested()
 	assert.Equal(3, len(apiResp.Tasks))
@@ -60,9 +60,43 @@ func TestGetTask(t *testing.T) {
 	}
 	apiResp = new(GetTaskResponse)
 	err = s.GetTask(apiReq, apiResp)
-	assert.Equal(nil, err)
+	assert.Nil(err)
 
 	assert.Equal(2, len(apiResp.Tasks))
 	assert.True(apiResp.Tasks[0].RootNode.Equal(multipleNested[1].RootNode))
 	assert.True(apiResp.Tasks[1].RootNode.Equal(multipleNested[1].RootNode))
+}
+
+// Tests GetTask's behavior when a nonexistent task ID is given
+func TestGetTask_Nonexistent(t *testing.T) {
+	t.Parallel()
+	assert := assert.New(t)
+
+	s, cleanup := NewServerWithTestdata()
+	defer cleanup()
+
+	// Malformatted line ID
+	apiReq := new(GetTaskRequest)
+	apiReq.TaskIDs = []string{"shmooooooooooooo"}
+	apiResp := new(GetTaskResponse)
+	err := s.GetTask(apiReq, apiResp)
+	assert.NotNil(err)
+
+	// Nonexistent line
+	apiReq.TaskIDs = []string{string(common.GetLineID("make_pasta", "fhghwhgads"))}
+	err = s.GetTask(apiReq, apiResp)
+	assert.NotNil(err)
+
+	// Line that exists but doesn't correspond to a task
+	apiReq.TaskIDs = []string{string(common.GetLineID("multiple_nested", "\tsubtask 0"))}
+	err = s.GetTask(apiReq, apiResp)
+	assert.NotNil(err)
+
+	// One task that exists, and one that doesn't
+	apiReq.TaskIDs = []string{
+		string(common.GetLineID("make_pasta", "make pasta")),
+		string(common.GetLineID("make_pasta", "burn self on pot")),
+	}
+	err = s.GetTask(apiReq, apiResp)
+	assert.NotNil(err)
 }
